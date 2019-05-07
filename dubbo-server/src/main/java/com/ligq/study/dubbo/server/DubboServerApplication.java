f2b348c6edc2b691b13198e786a35974ca4c80e0
ligq <liguoqiang@ebyf.com.cn>
2019-05-07 11:13:11 +0800

    init project

---
+++ dubbo-server/src/main/java/com/ligq/study/dubbo/server/DubboServerApplication.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019. EverBright Yunfu Internet CO.,LTD. All rights reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * Proprietary and confidential.
+ */
+
+package com.ligq.study.dubbo.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * class
+ *
+ * @author LiGuoQiang
+ * @description
+ * @date 2019/5/7 0007,10:49
+ */
+@Slf4j
+@SpringBootApplication
+public class DubboServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboServerApplication.class, args);
+    }
+}
