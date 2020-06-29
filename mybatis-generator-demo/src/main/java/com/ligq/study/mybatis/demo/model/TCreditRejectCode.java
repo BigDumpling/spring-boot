c95d2a4ecae99f78ecdd57ed39634b995d701b73
ligq6 <ligq6@newhope.cn>
2020-06-29 20:50:55 +0800

    20200629

---
+++ mybatis-generator-demo/src/main/java/com/ligq/study/mybatis/demo/model/TCreditRejectCode.java
@@ -0,0 +1,83 @@
+package com.ligq.study.mybatis.demo.model;
+
+public class TCreditRejectCode {
+    private String seqId;
+
+    private String productCode;
+
+    private String productName;
+
+    private String rejectCode;
+
+    private String rejectDesc;
+
+    private String rejectDescFront;
+
+    private Long createTime;
+
+    private String createUser;
+
+    public String getSeqId() {
+        return seqId;
+    }
+
+    public void setSeqId(String seqId) {
+        this.seqId = seqId;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getRejectCode() {
+        return rejectCode;
+    }
+
+    public void setRejectCode(String rejectCode) {
+        this.rejectCode = rejectCode;
+    }
+
+    public String getRejectDesc() {
+        return rejectDesc;
+    }
+
+    public void setRejectDesc(String rejectDesc) {
+        this.rejectDesc = rejectDesc;
+    }
+
+    public String getRejectDescFront() {
+        return rejectDescFront;
+    }
+
+    public void setRejectDescFront(String rejectDescFront) {
+        this.rejectDescFront = rejectDescFront;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+}
