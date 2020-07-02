3c6644e77966bf19c09e5f0b916ca95bded44cd5
ligq6 <ligq6@newhope.cn>
2020-07-02 19:00:26 +0800

    20200702

---
+++ spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/config/ConfigTest.java
@@ -0,0 +1,14 @@
+//package com.ligq.study.sping.ioc.demo.config;
+//
+//import org.springframework.context.annotation.Import;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class ConfigTest {
+//
+//    @Import(B.class)
+//    class A {}
+//
+//    @Import(A.class)
+//    class B {}
+//}
---
+++ spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/config/IocDemoBeanFactoryPostProcessor.java
@@ -0,0 +1,14 @@
+package com.ligq.study.sping.ioc.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+@Slf4j
+public class IocDemoBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        log.info("----- IocDemoBeanFactoryPostProcessor -----");
+    }
+}
