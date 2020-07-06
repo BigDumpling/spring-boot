d7074c9b87bca4060c95c6bbd139fa6f9854603b
ligq6 <ligq6@newhope.cn>
2020-07-06 19:54:00 +0800

    20200706

--- spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/service/Demo1Service.java
+++ spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/service/Demo1Service.java
@@ -1,11 +1,25 @@
 package com.ligq.study.sping.ioc.demo.service;
 
+import com.ligq.study.sping.ioc.demo.config.IocDemoBeanFactoryPostProcessor2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class Demo1Service {
+
+//    private IocDemoBeanFactoryPostProcessor2 iocDemoBeanFactoryPostProcessor2;
+//
+    public Demo1Service(){}
+
+
+//    @Autowired
+//    public Demo1Service(IocDemoBeanFactoryPostProcessor2 ioc){
+//        log.info("IocDemoBeanFactoryPostProcessor2 ioc ");
+//    }
+
+
 
     public void demo() {
         log.info("Demo1Service demo");
