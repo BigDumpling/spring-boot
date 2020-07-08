c6400607fe869efcbfe940fc54a1861f8f5c918e
ligq6 <ligq6@newhope.cn>
2020-07-08 18:27:40 +0800

    20200708

--- spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/service/Demo1Service.java
+++ spring-ioc-demo/src/main/java/com/ligq/study/sping/ioc/demo/service/Demo1Service.java
@@ -2,12 +2,15 @@ package com.ligq.study.sping.ioc.demo.service;
 
 import com.ligq.study.sping.ioc.demo.config.IocDemoBeanFactoryPostProcessor2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @Service
-public class Demo1Service {
+public class Demo1Service implements InitializingBean {
 
     @Autowired
     private IocDemoBeanFactoryPostProcessor2 iocDemoBeanFactoryPostProcessor2;
@@ -25,7 +28,13 @@ public class Demo1Service {
 //        log.info("IocDemoBeanFactoryPostProcessor2 ioc, name ");
 //    }
 
+    @PostConstruct
     public void demo() {
-        log.info("Demo1Service demo");
+        log.info("PostConstruct Demo1Service demo");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("InitializingBean Demo1Service demo");
     }
 }
