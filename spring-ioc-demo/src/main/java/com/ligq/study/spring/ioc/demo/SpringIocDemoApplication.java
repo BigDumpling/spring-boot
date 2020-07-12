3aec947daa6d0d7361d9053f32a8ea1158c96a25
Bigdumpling <smallqiang13@163.com>
2020-07-13 07:56:54 +0800

    20200712

--- spring-ioc-demo/src/main/java/com/ligq/study/spring/ioc/demo/SpringIocDemoApplication.java
+++ spring-ioc-demo/src/main/java/com/ligq/study/spring/ioc/demo/SpringIocDemoApplication.java
@@ -6,15 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Slf4j
 @SpringBootApplication
 @EnableAspectJAutoProxy
-//@EnableTransactionManagement
+@EnableTransactionManagement
 public class SpringIocDemoApplication {
 
     public static void main(String[] args) {
+        new ArrayList<>(Arrays.asList());
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(DemoConfig.class);
         Demo1Service demo1Service = (Demo1Service) applicationContext.getBean("demo1Service");
         demo1Service.demo();
