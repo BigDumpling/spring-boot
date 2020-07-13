b981ea596b4df8fb4c7a86b3a95c67810ec99372
ligq6 <ligq6@newhope.cn>
2020-07-13 18:50:47 +0800

    20200713

--- spring-ioc-demo/src/main/java/com/ligq/study/spring/ioc/demo/SpringIocDemoApplication.java
+++ spring-ioc-demo/src/main/java/com/ligq/study/spring/ioc/demo/SpringIocDemoApplication.java
@@ -3,6 +3,7 @@ package com.ligq.study.spring.ioc.demo;
 import com.ligq.study.spring.ioc.demo.config.DemoConfig;
 import com.ligq.study.spring.ioc.demo.service.Demo1Service;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -19,9 +20,12 @@ import java.util.Arrays;
 public class SpringIocDemoApplication {
 
     public static void main(String[] args) {
-        new ArrayList<>(Arrays.asList());
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(DemoConfig.class);
-        Demo1Service demo1Service = (Demo1Service) applicationContext.getBean("demo1Service");
-        demo1Service.demo();
+//        new ArrayList<>(Arrays.asList());
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(DemoConfig.class);
+//        Demo1Service demo1Service = (Demo1Service) applicationContext.getBean("demo1Service");
+//        demo1Service.demo();
+
+
+        new SpringApplication(SpringIocDemoApplication.class).run();
     }
 }
