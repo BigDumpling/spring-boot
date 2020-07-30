156129089be6f60b11574741bedbd21e2dd00bdd
ligq6 <ligq6@newhope.cn>
2020-07-30 18:23:46 +0800

    20200730

---
+++ mybatis-generator-demo/src/main/java/com/ligq/study/mybatis/demo/model/TNoticeTemplate.java
@@ -0,0 +1,103 @@
+package com.ligq.study.mybatis.demo.model;
+
+public class TNoticeTemplate {
+    private String id;
+
+    private String templateKey;
+
+    private String title;
+
+    private String createUser;
+
+    private Long createTime;
+
+    private String updateUser;
+
+    private Long updateTime;
+
+    private String templateStatus;
+
+    private String remark;
+
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTemplateKey() {
+        return templateKey;
+    }
+
+    public void setTemplateKey(String templateKey) {
+        this.templateKey = templateKey;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getTemplateStatus() {
+        return templateStatus;
+    }
+
+    public void setTemplateStatus(String templateStatus) {
+        this.templateStatus = templateStatus;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
